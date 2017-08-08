20735c8f94ded6697ae1e5af40250d72e91a233a
gilesknap <giles.knap@diamond.ac.uk>
2017-08-08 23:27:06 +0100

    Adding --index-only

--- PhotoInfo.py
+++ PhotoInfo.py
@@ -6,18 +6,25 @@ import gdata.photos.service
 
 
 class PhotoInfo:
+    PHOTOS_QUERY = '/data/feed/api/user/default/albumid/{0}?kind=photo'
+
     def __init__(self):
         self.gdata_client = None
         self.credentials = None
         self.auth2token = None
 
     def get_albums(self):
-        albums = self.gdata_client.GetUserFeed()
+        albums = self.gdata_client.GetUserFeed(limit=10)
         for album in albums.entry:
             print('title: %s, number of photos:"'
                   ' %s, id: %s' % (album.title.text,
                                    album.numphotos.text,
                                    album.gphoto_id.text))
+            q = PhotoInfo.PHOTOS_QUERY.format(album.gphoto_id.text)
+            if album.title.text == 'Test123':
+                photos = self.gdata_client.GetFeed(q)
+                for photo in photos.entry:
+                    print(photo)
         print('Album count %d' % len(albums.entry))
         return albums
 
