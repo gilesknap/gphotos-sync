cb01bda69c29041a6a0991c34f77a847cd9b4f76
Giles Knap <giles.knap@diamond.ac.uk>
2017-09-06 12:46:16 +0100

    gdata ouath2 token refresh now fully working

--- Utils.py
+++ Utils.py
@@ -70,24 +70,19 @@ def timestamp_to_date(time_secs, hour_offset=0):
 # Here we patch over their patch to fix
 # error 'TypeError: new_request() takes exactly 1 argument (4 given)'
 def patch_http_client(oauth, client, request_orig2):
-    class DummyOldStyleRequest:
-        def __init__(self, h):
-            self.headers = h
-
     client.auth_token = oauth
 
     def new_request2(*args, **k_args):
         response = request_orig2(*args, **k_args)
-        # I've added 403 because I have just seen it on token expiry.
-        # Getting 403 is incorrect so it remains to be seen if this will help
         if response.status == 401 or response.status == 403:
-            print '\ntoken refresh\n'
             refresh_response = oauth._refresh(request_orig2)
             if oauth._invalid:
                 return refresh_response
             else:
-                req = DummyOldStyleRequest(k_args['headers'])
-                oauth.modify_request(req)
+                print('\n token refresh: {}\n'.format(oauth.access_token))
+                new_h = '{}{}'.format('Bearer ', oauth.access_token)
+                client.additional_headers['Authorization'] = new_h
+                k_args['headers']['Authorization'] = new_h
             return request_orig2(*args, **k_args)
         else:
             return response
