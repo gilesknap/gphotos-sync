161e895aa843cb2edac1db37bde397da95744ddf
David Grant <davidgrant@gmail.com>
2018-03-13 22:41:49 -0700

    use /usr/bin/env python2 everywhere, not hard-coded /usr/bin/python

--- gphotos/LocalData.py
+++ gphotos/LocalData.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # coding: utf8
 import os.path
 import sqlite3 as lite
--- gphotos/Utils.py
+++ gphotos/Utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # coding: utf8
 from __future__ import division
 
