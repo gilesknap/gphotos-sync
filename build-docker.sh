171cd2ba67ca6cab4325e901f74aa6e68e7a9402
giles <gilesknap@gmail.com>
2019-08-14 08:57:40 +0100

    fix build-docker environment checks

--- build-docker.sh
+++ build-docker.sh
@@ -2,8 +2,8 @@
 set -x
 
 command -v docker >/dev/null 2>&1 || { echo >&2 "Docker CLI is required."; exit 1; }
-[[ -z "${DOCKER_USERNAME}" ]] || { echo >&2 "DOCKER_USERNAME is not set."; exit 1; }
-[[ -z "${DOCKER_PASSWORD}" ]] || { echo >&2 "DOCKER_PASSWORD is not set."; exit 1; }
+[[ -z "${DOCKER_USERNAME}" ]] && { echo >&2 "DOCKER_USERNAME is not set."; exit 1; }
+[[ -z "${DOCKER_PASSWORD}" ]] && { echo >&2 "DOCKER_PASSWORD is not set."; exit 1; }
 
 docker build . \
     -t gilesknap/gphotos-sync:latest \
