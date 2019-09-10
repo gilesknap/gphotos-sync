dc8c5683c3022acb52c0cce798c392737979da22
giles <gilesknap@gmail.com>
2019-09-10 22:20:34 +0100

    add docker versioning

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.rst", "rb") as f:
 
 setup(
     name=module_name,
-    version='2.10',
+    version='2.10.1',
     python_requires='>=3.6',
     license='MIT',
     platforms=['Linux', 'Windows', 'Mac'],
