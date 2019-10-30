164844f031ebb1d0f686685fa464ac846e14b2e3
giles <gilesknap@gmail.com>
2019-10-30 07:18:57 +0000

    version 2.10.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.rst", "rb") as f:
 
 setup(
     name=module_name,
-    version='2.10.1',
+    version='2.10.2',
     python_requires='>=3.6',
     license='MIT',
     platforms=['Linux', 'Windows', 'Mac'],
