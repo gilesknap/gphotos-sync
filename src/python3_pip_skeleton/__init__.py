d97fd677d6e7e666aaf6c8d1e079d338e75e00db
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-11-11 16:06:36 +0000

    Use importlib.metadata to get package version

--- src/python3_pip_skeleton/__init__.py
+++ src/python3_pip_skeleton/__init__.py
@@ -1,12 +1,6 @@
-try:
-    # Use live version from git
-    from setuptools_scm import get_version
+from importlib.metadata import version
 
-    # Warning: If the install is nested to the same depth, this will always succeed
-    __version__ = get_version(root="../../", relative_to=__file__)
-    del get_version
-except (ImportError, LookupError):
-    # Use installed version
-    from ._version import __version__
+__version__ = version("python3-pip-skeleton")
+del version
 
 __all__ = ["__version__"]
