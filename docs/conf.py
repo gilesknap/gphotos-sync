6f588b906eef4e134885fa9d29b031584bda3a06
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-10-27 11:02:47 +0100

    (#153) Fix docs build by providing default arg

--- docs/conf.py
+++ docs/conf.py
@@ -173,6 +173,7 @@ html_theme_options = {
             "url": f"https://github.com/{github_user}/{github_repo}/releases",
         }
     ],
+    "navigation_with_keys": False,
 }
 
 # A dictionary of values to pass into the template engine’s context for all pages
