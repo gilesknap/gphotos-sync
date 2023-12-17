fa057fd215fd6ac8d580b4e5562ca0db36f87340
Giles Knap <gilesknap@gmail.com>
2023-12-17 15:00:02 +0000

    more merge fixes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
 import subprocess
 import sys
 
-from photos_sync import __version__
+from gphotos_sync import __version__
 
 
 def test_cli_version():
-    cmd = [sys.executable, "-m", "photos_sync", "--version"]
+    cmd = [sys.executable, "-m", "gphotos_sync", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
