136534fdd8270f3b4904c2c4041f8eaf6620d9d7
gilesknap <gilesknap@users.noreply.github.com>
2024-07-13 21:36:25 +0000

    deploy: 8442308eb5c1aea87292fb637dd75504009fd2d8

---
+++ fix-488/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.2.5.dev7+g8442308',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
